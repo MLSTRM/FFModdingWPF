d1309b1ceb4f6c5f6b7be0e0c4640ed93d425f41
Bartz24 <bartz24games@gmail.com>
2024-04-27 09:34:31 -0400

    [0.8.1.5] Fix AP lua script

--- FF12Rando/bin/data/scriptsAP/RandoArchipelago.lua
+++ FF12Rando/bin/data/scriptsAP/RandoArchipelago.lua
@@ -29,7 +29,7 @@ local function defineAddItem()
       sub rsp,0x38
       lea rax,[%add_args%]
       movzx ecx,word ptr [rax]
-      movsx edx,dword ptr [rax+0x02]
+      mov edx,dword ptr [rax+0x02]
       xor r8d,r8d
       lea r9d,[r8+0x01]
       mov [rsp+0x20],r9d
